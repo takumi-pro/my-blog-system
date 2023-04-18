1a71b95179e75a6d0cf9c40b5a5a9cbebaf1b80f
takumi-pro <takumidiary.0927@gmail.com>
2023-04-18 21:51:24 +0900

    feat: :sparkles: dark/lightモード

--- src/components/ChangeThemeButton/ChangeThemeButton.tsx
+++ src/components/ChangeThemeButton/ChangeThemeButton.tsx
@@ -1,12 +1,16 @@
 import { useTheme } from 'next-themes';
 import React from 'react';
 
+import style from './ChangeThemeButton.module.css';
+
 const ChangeThemeButton = () => {
   const { theme, setTheme } = useTheme();
 
   return (
     <>
-      <button onClick={() => setTheme('light')}>Light Mode</button>
+      <button className={`${style.bg}`} onClick={() => setTheme('light')}>
+        Light Mode
+      </button>
       <button onClick={() => setTheme('dark')}>Dark Mode</button>
     </>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
+    <ThemeProvider attribute="data-theme" defaultTheme="dark">
       <Component {...pageProps} />
     </ThemeProvider>
   );
