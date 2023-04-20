cb31d494f308953bd6493d6a69246d5025de8d8f
takumi-pro <takumidiary.0927@gmail.com>
2023-04-20 21:38:50 +0900

    feat: :sparkles: テーマ変更ボタン作成

--- src/components/ChangeThemeButton/ChangeThemeButton.tsx
+++ src/components/ChangeThemeButton/ChangeThemeButton.tsx
@@ -1,18 +1,28 @@
 import { useTheme } from 'next-themes';
 import React from 'react';
+import { BsFillMoonFill, BsSun } from 'react-icons/bs';
 
 import style from './ChangeThemeButton.module.css';
 
 const ChangeThemeButton = () => {
   const { theme, setTheme } = useTheme();
 
+  const handleClickChangeTheme = () => {
+    if (theme === 'light') setTheme('dark');
+    if (theme === 'dark') setTheme('light');
+  };
+
   return (
-    <>
-      <button className={`${style.bg}`} onClick={() => setTheme('light')}>
-        Light Mode
-      </button>
-      <button onClick={() => setTheme('dark')}>Dark Mode</button>
-    </>
+    <div
+      className={style['theme-button-container']}
+      onClick={handleClickChangeTheme}
+    >
+      {theme === 'dark' ? (
+        <BsSun className={style.light} />
+      ) : (
+        <BsFillMoonFill className={style.dark} />
+      )}
+    </div>
   );
 };
 
