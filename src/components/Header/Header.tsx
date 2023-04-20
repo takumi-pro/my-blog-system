038b8760582d72272c7a0c7f28b4ebb2dfc13707
takumi-pro <takumidiary.0927@gmail.com>
2023-04-20 21:29:19 +0900

    feat: :sparkles: ヘッダー作成

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,38 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 import style from './Header.module.css';
-import topImage from '../../images/takumi.png';
+import ChangeThemeButton from '../ChangeThemeButton/ChangeThemeButton';
+import { CircleIcon } from '../CircleIcon/CircleIcon';
 
 export const Header = () => {
   return (
     <header className={style.header}>
-      <div>
-        <Image src={topImage} alt="top-image..." />
+      <div className={style['header-inner']}>
+        <div className={style['left-side-container']}>
+          <CircleIcon />
+          <nav className={style.nav}>
+            <ul className={style['list-container']}>
+              <li>
+                <Link className={style.link} href={'#'}>
+                  article
+                </Link>
+              </li>
+              <li>
+                <Link className={style.link} href={'#'}>
+                  about
+                </Link>
+              </li>
+              <li>
+                <Link className={style.link} href={'#'}>
+                  works
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <ChangeThemeButton />
       </div>
-      <nav>
-        <ul>
-          <li>
-            <Link href={'#'}>Article</Link>
-          </li>
-          <li>
-            <Link href={'#'}>About</Link>
-          </li>
-          <li>
-            <Link href={'#'}>Works</Link>
-          </li>
-        </ul>
-      </nav>
-      <div>dark/light icon</div>
     </header>
   );
 };
