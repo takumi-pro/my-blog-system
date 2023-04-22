921dc6377b0939b8cb9a0217db988d91201872db
takumi-pro <takumidiary.0927@gmail.com>
2023-04-22 21:41:36 +0900

    feat: :sparkles: 記事一覧
    Closes

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import React from 'react';
 
+import { EyeCatch } from '@/components/EyeCatch/EyeCatch';
 import { Header } from '@/components/Header/Header';
 
 import style from '../styles/Articles.module.css';
@@ -15,42 +17,36 @@ function Articles() {
             ここにカテゴリが入るよ
           </div>
           <div className={style['articles-container']}>
-            <div className={style.article}>
-              <div className={style['eye-catch']}></div>
+            <Link href={'#'} className={style.article}>
+              <EyeCatch emoji="📝" />
+              <div className={style.contents}>
+                <p className={style.title}>DDNSについて復習</p>
+                <small className={style.date}>2023/4/20</small>
+              </div>
+            </Link>
+            <Link href={'#'} className={style.article}>
+              <EyeCatch emoji="㊗️" />
+              <div className={style.contents}>
+                <p className={style.title}>〇〇に合格しました</p>
+                <small className={style.date}>2023/4/20</small>
+              </div>
+            </Link>
+            <Link href={'#'} className={style.article}>
+              <EyeCatch emoji="💻" />
               <div className={style.contents}>
                 <p className={style.title}>
-                  NextとmicroCMSでブログ開発してみた
+                  【Next.js】Next.jsとmicroCMSを使って自作ブログを構築してみたら案外簡単でとても驚いた話
                 </p>
                 <small className={style.date}>2023/4/20</small>
               </div>
-            </div>
-            <div className={style.article}>
-              <div className={style['eye-catch']}></div>
+            </Link>
+            <Link href={'#'} className={style.article}>
+              <EyeCatch emoji="📝" />
               <div className={style.contents}>
-                <p className={style.title}>
-                  NextとmicroCMSでブログ開発してみた
-                </p>
+                <p className={style.title}>DDNSについて復習</p>
                 <small className={style.date}>2023/4/20</small>
               </div>
-            </div>
-            <div className={style.article}>
-              <div className={style['eye-catch']}></div>
-              <div className={style.contents}>
-                <p className={style.title}>
-                  NextとmicroCMSでブログ開発してみた
-                </p>
-                <small className={style.date}>2023/4/20</small>
-              </div>
-            </div>
-            <div className={style.article}>
-              <div className={style['eye-catch']}></div>
-              <div className={style.contents}>
-                <p className={style.title}>
-                  NextとmicroCMSでブログ開発してみた
-                </p>
-                <small className={style.date}>2023/4/20</small>
-              </div>
-            </div>
+            </Link>
           </div>
         </div>
       </main>
