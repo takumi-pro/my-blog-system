ecf1671c8367f68c9b08074254a5004bae830efe
takumi-pro <takumidiary.0927@gmail.com>
2023-04-24 20:36:41 +0900

    feat: :sparkles: ホーム画面のマークアップ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
-import ChangeThemeButton from '@/components/ChangeThemeButton/ChangeThemeButton';
+import { AiFillGithub } from 'react-icons/ai';
+import { BsTwitter } from 'react-icons/bs';
+
+import { CircleIcon } from '@/components/CircleIcon/CircleIcon';
 import { Header } from '@/components/Header/Header';
 
+import style from '../styles/Home.module.css';
+
 export default function Home() {
+  const imageStyle = {
+    width: '80px',
+    height: '80px',
+  };
   return (
     <>
       <Header />
-      <ChangeThemeButton />
+      <main className={style['main-container']}>
+        <div className={style['main-inner']}>
+          <div className={style['self-name-container']}>
+            <CircleIcon imageStyle={imageStyle} />
+            <p>Takumi / Software Enginner</p>
+          </div>
+          <div className={style['self-introduction']}>
+            都内のIT企業でソフトウェアエンジニアをしているtakumiです。
+            {/* TODO: 文言検討 */}
+          </div>
+          <h3 className={style.heading3}>Link</h3>
+          <div className={style['each-content']}>
+            <AiFillGithub />
+            <BsTwitter />
+          </div>
+        </div>
+      </main>
     </>
   );
 }
