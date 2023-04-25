0f01f0f428a67c09ac638d8489cc449bcc539c20
takumi-pro <takumidiary.0927@gmail.com>
2023-04-26 00:29:09 +0900

    feat: :sparkles: フッター作成

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <div className={style['footer-container']}>
+      <span>©️ 2023 takumi</span>
+    </div>
+  );
+};
